cf3882c1c91a16a7e1dd5e1e088235affffc4a3b
Valera-Neg <nehovora.val@gmail.com>
2022-09-12 15:05:51 -0400

    first commit

---
+++ checkForFactor.js
@@ -0,0 +1,14 @@
+function checkForFactor (base, factor) {
+  return base % factor === 0 ? true : false;
+}
+
+
+console.log(checkForFactor(10,2)) //> true
+console.log(checkForFactor(63,7)) //> true
+console.log(checkForFactor(2450,5)) //> true
+console.log(checkForFactor(24612,3)) //> true
+console.log("********************************")
+console.log(checkForFactor(9,2)) //> false
+console.log(checkForFactor(653,7)) //> false
+console.log(checkForFactor(2453,5)) //> false
+console.log(checkForFactor(24617,3)) //> false
